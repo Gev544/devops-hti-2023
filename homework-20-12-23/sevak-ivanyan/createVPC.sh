14837ff2c632489d5456e0ad0c85a10b1b32eff7
sevakivanyan <sevakivanyan@gmail.com>
2023-12-27 12:33:51 +0400

    Implementing EC2 Instance Creation

--- homework-20-12-23/sevak-ivanyan/createVPC.sh
+++ homework-20-12-23/sevak-ivanyan/createVPC.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 region="us-east-1"
+keyPairName="*"
+amiId="ami-*"
 
 echo "start create vpc"
 vpc=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query Vpc.VpcId --output text)
@@ -29,6 +31,22 @@ echo "end open access"
 echo "start assign sbn rte"
 aws ec2 associate-route-table --subnet-id $subnet --route-table-id $routeTable
 echo "end assign sbn rte"
+
+echo "start creating EC2 instance"
+instance=$(aws ec2 run-instances --image-id $amiId --instance-type t2.micro --key-name $keyPairName --subnet-id $subnet --query 'Instances[0].InstanceId' --output text --region $region)
+echo "end EC2 instance created $instance"
+
+echo "start running EC2 instance"
+aws ec2 wait instance-running --instance-ids $instance --region $region
+echo "EC2 instance running"
+
+echo "elastic IP allocate"
+publicIp=$(aws ec2 allocate-address --domain vpc --query 'PublicIp' --output text --region $region)
+echo "elastic IP allocated $publicIp"
+
+echo "associate the IP with the instance"
+aws ec2 associate-address --instance-id $instance --public-ip $publicIp --region $region
+echo "elastic IP associated"
 
 
 
