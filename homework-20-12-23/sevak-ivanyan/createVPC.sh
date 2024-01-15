7e56be23892d475d9177ed2f1b1365872d89f53f
sevakivanyan <sevakivanyan@gmail.com>
2024-01-15 10:57:09 +0400

    Adding tag creation

--- homework-20-12-23/sevak-ivanyan/createVPC.sh
+++ homework-20-12-23/sevak-ivanyan/createVPC.sh
@@ -6,14 +6,17 @@ amiId="ami-*"
 
 echo "start create vpc"
 vpc=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query Vpc.VpcId --output text)
+aws ec2 create-tags --resources "$vpc" --tags Key=Name,Value=firstVpc
 echo "end create vpcId $vpc"
 
 echo "start create sbn"
 subnet=$(aws ec2 create-subnet --vpc-id $vpc --cidr-block 10.0.0.1/24 --availability-zone ${region}a --query 'Subnet.SubnetId' --output text)
+aws ec2 create-tags --resources "$subnet" --tags Key=Name,Value=firstSubnet
 echo "end create sbnID $subnet"
 
 echo "start create igw"
 gateway=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text)
+aws ec2 create-tags --resources "$gateway" --tags Key=Name,Value=firstGateway
 echo "end create igwID $gateway"
 
 echo "start attach vpc igw"
@@ -22,6 +25,7 @@ echo "end attach vpc igw"
 
 echo "start create rte"
 routeTable=$(aws ec2 create-route-table --vpc-id $vpc --query 'RouteTable.RouteTableId' --output text)
+aws ec2 create-tags --resources "$routeTable" --tags Key=Name,Value=firstRouteTable
 echo "end create rteId $routeTable"
 
 echo "start open access"
@@ -34,6 +38,7 @@ echo "end assign sbn rte"
 
 echo "start creating EC2 instance"
 instance=$(aws ec2 run-instances --image-id $amiId --instance-type t2.micro --key-name $keyPairName --subnet-id $subnet --query 'Instances[0].InstanceId' --output text --region $region)
+aws ec2 create-tags --resources "$instance" --tags Key=Name,Value=firstInstance
 echo "end EC2 instance created $instance"
 
 echo "start running EC2 instance"
