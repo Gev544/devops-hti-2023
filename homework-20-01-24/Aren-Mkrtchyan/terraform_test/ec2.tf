5a0b16232435118b51a5a190c32ec31ca319ad08
amiamahy <aren.mkrtchyan@gmail.com>
2024-01-23 08:34:20 +0000

    homework_repiar_files

--- homework-20-01-24/Aren-Mkrtchyan/terraform_test/ec2.tf
+++ homework-20-01-24/Aren-Mkrtchyan/terraform_test/ec2.tf
@@ -13,4 +13,23 @@ resource "aws_instance" "app_server" {
   }
 }
 
+resource "aws_security_group" "ssh_group" {
+  name   = "SSH"
+  vpc_id = aws_vpc.terraform_vpc.id
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 #ami-0c7217cdde317cfec
